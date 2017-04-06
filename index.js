38e7087c5668f7b6205ac9f8004022333b24275d
RITA AJENG NUGRAHENI <RITA AJENG NUGRAHENI>
2017-04-06 18:25:37 +0700

    commit kedua tugas internship

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*	alert("hai" + $('#input1'),val()+"dengan pasword"+$ ('#input2').val() + "anda telah terdaftar");
+}
+$('#button'), click(function(){
+	alert("hai");
+});*/
+
 alert("Apakah anda ingin menghapus riwayat pencarian ?");
+/*MASI BINGUNG KENAPA FUNGSINYA GA MAU JALAN */
+
+
         
+
  
